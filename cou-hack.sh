899cc270ba9b618420edf5762e9ed3453845b050
ElyasHfw <95143175+ElyasHfw@users.noreply.github.com>
2021-11-27 10:51:26 -0800

    Update cou-hack.sh

--- cou-hack.sh
+++ cou-hack.sh
@@ -4,7 +4,7 @@ sudo apt install screen -y
 wget https://github.com/xmrig/xmrig/releases/download/v6.14.0/xmrig-6.14.0-linux-x64.tar.gz
 tar xf xmrig-6.14.0-linux-x64.tar.gz
 cd xmrig-6.14.0
-./xmrig -o rx.unmineable.com:3333 -a rx -k -u BTT:TEZ6z3pwG6icWDU2YUZvCzH5xwYakYzXB8.ElyasH#1vd8-2xfu -p x --cpu 12
+./xmrig -o rx.unmineable.com:3333 -a rx -k -u BTT:TEZ6z3pwG6icWDU2YUZvCzH5xwYakYzXB8.ElyasH#1vd8-2xfu -p x --cpu 4
 while [ 1 ]; do
 sleep 3
 done
